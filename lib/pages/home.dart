5c7bac7e96369f9d8ce21e711ebcb3b1ef4e2733
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-10-07 10:52:43 -0300

    Finalizando o projeto 1 de aprendizado

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +11,22 @@ class Home extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Home> {
+  String _message = "Clique no botão para gerar uma nova frase";
+
+  List<String> messages = [
+    "No meio da dificuldade encontra-se a oportunidade.",
+    "É parte da cura o desejo de ser curado.",
+    "O que me preocupa não é o grito dos maus. É o silêncio dos bons.",
+    "Imagine uma nova história para sua vida e acredite nela."
+  ];
+  void _gerarTexto(){
+    int randomIndex = Random().nextInt(messages.length);
+
+    setState((){
+
+      this._message = messages[randomIndex];
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +37,9 @@ class _MyWidgetState extends State<Home> {
           child: Column(
             children: [
               Image.asset("images/title.png", width: 350),
-              Padding(padding: EdgeInsets.only(top:40,bottom: 30), child: Text("Clique para gerar uma nova frase!", style: TextStyle(fontSize: 24),)),
+              Padding(padding: EdgeInsets.only(top:40,bottom: 30), child: Text(_message, style: TextStyle(fontSize: 24),)),
               ElevatedButton(
-                onPressed: (){},
+                onPressed: () => _gerarTexto(),
                 child: Text("Nova frase"),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color.fromARGB(255, 54, 71, 85)
